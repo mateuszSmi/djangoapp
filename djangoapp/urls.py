736ab7171dbc890de4da3b55d3305c80641d6f42
mateuszSmi <mateusz.smigala@lo1.sandomierz.pl>
2020-10-01 13:34:52 +0200

    kolejna leckja django

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('osoby.urls')),
     path('osoby/', include('osoby.urls')),
     path('admin/', admin.site.urls),
 ]
